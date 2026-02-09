66d1145b20dea9ef975d8600ea2f996e1dd3708f
TGELITEGAMER138 <tgelitegamer138@gmail.com>
2026-02-09 13:13:17 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'Beta Studio'
+description 'Blue-Series Loading Screen'
+
+lua54 'yes'
+version '1.0' 
+
+loadscreen 'public/index.html'
+loadscreen_cursor 'yes'
+loadscreen_manual_shutdown 'yes' --sorun çıkarıtras sil--
+
+
+files {
+	'public/**',
+
+}
